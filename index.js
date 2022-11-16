79e75f74971f70c47de67de350f4383c70ac4176
harsh116 <71572299+harsh116@users.noreply.github.com>
2022-11-17 03:26:03 +0530

    corrected public path

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 // });
 app.use(express.json());
 app.use(cors(corsOptions));
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 app.post("/getList", getList);
 
