162760b258e6dbfb7d9ab03e7a3ce1730e9a9a7a
harsh116 <71572299+harsh116@users.noreply.github.com>
2023-01-11 11:45:13 +0530

    added deta api

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
-const videoLinkHOST = "https://ill-lion-jumpsuit.cyclic.app";
+// const videoLinkHOST = "https://ill-lion-jumpsuit.cyclic.app";
 // const videoLinkHOST = "http://localhost:8082";
+const videoLinkHOST='https://kjw5zb.deta.dev'
 
 module.exports = { videoLinkHOST };
